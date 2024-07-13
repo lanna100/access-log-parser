c9edbd879765e33eb6e985a2f0ad71e940cdd20e
Anna <m.a.s.1126@yandex.ru>
2024-07-13 23:56:58 +0300

    arithmetic operations added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int num1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int num2 = new Scanner(System.in).nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1*num2;
+        double quotient = (double)num1/num2;
+
+        System.out.println("Сумма этих чисел = " + (sum));
+        System.out.println("Разность этих чисел = " + (difference));
+        System.out.println("Произведение этих чисел = " + (product));
+        System.out.println("Частное этих чисел = " + (quotient));
     }
 }
